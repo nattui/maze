8676794c6f808d045d8e741037e3481a6644cca9
Jasmine Mai <jasminemai@Jasmines-MacBook-Pro.local>
2018-04-25 17:53:53 -0700

    GitHub Test

--- MazeGenerator.java
+++ MazeGenerator.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class MazeGenerator {
-
+// here
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
