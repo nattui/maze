ff3ad7420a1adff9bfe70142e52cc644802b7d32
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2018-05-04 02:11:00 -0700

    Updated JavaDocs

--- Cell.java
+++ Cell.java
@@ -10,23 +10,57 @@ public class Cell {
 	private Cell node;
 	private int x;
 	private int y;
-	
+
+	/**
+	 * Constructor has the location of each Cell
+	 * 
+	 * @param x The x-coordinate
+	 * @param y The y-coordinate
+	 */
 	public Cell(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+
+	/**
+	 * Gets the x-coordinate
+	 * 
+	 * @return x The x-coordinate
+	 */
 	public int getx() {
 		return x;
 	}
+
+	/**
+	 * Gets the y-coordinate
+	 * 
+	 * @return y The y-coordinate
+	 */
 	public int gety() {
 		return y;
 	}
+
+	/**
+	 * Gets the next node
+	 * 
+	 * @return node The next node
+	 */
 	public Cell getNext() {
 		return node;
 	}
+
+	/**
+	 * Sets the next node
+	 * 
+	 * @param node The next node being set
+	 */
 	public void setNext(Cell node) {
 		this.node = node;
 	}
+
+	/**
+	 * String Representation of the Cell class 
+	 */
 	public String toString() {
 		return "[" + x + ":" + y + "]";
 	}
